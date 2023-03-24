c297ccc9d0dcd027974c2678db90fb3ea47e7811
KatyaMamontova <rbkm572@gmail.com>
2023-03-24 14:15:42 +0400

    многочлен Ньютона работает через eval

--- main.js
+++ main.js
@@ -27,57 +27,87 @@ let N = 28;
     point(1.0, N),
 ] */
 
+const round = num => Math.round(num * 100) / 100;
+
 function createZeroMatrix(size) {
     return new Array(size).fill(0).map(row => new Array(size).fill(0));
 }
 
-function defineNewtonPoly(points) {
-    const n = points.length - 1;
-    const matrix = createZeroMatrix(n) //для разделенных разностей
-    let vectX = [];
-    let vectY = [];
-    points.forEach(point => {
-        vectX.push(point.x);
-        vectY.push(point.y);
-    });
-    for (let i = 0; i < n; i++) {
-        matrix[i][0] = (vectY[i + 1] - vectY[i]) / (vectX[i + 1] - vectX[i])
-    }
-    let num = 0;
-    for (let i = 1; i < n; i++) {
-        for (let j = 0; j < n - i; j++) {
-            // matrix[j][i] = num;
-            // num++
-            matrix[j][i] = (matrix[j + 1][i - 1] -  matrix[j][i - 1]) /  (vectX[i] - vectX[j])
-            console.log(`(${matrix[j + 1][i - 1]}-${matrix[j][i - 1]})/(${vectX[i]}-${vectX[j]})`)
-            console.log(matrix)
+const n = points.length;
+const vectX = [];
+const vectY = [];
+points.forEach(point => {
+    vectX.push(point.x);
+    vectY.push(point.y);
+});
 
+function defineDividedDifferences(points) {
+
+    const matrix = createZeroMatrix(n)
+    let num = 1;
+    for (let i = 0; i < n; i++) {               //столбец
+        for (let j = 0; j < n - i; j++) {       //строка
+            if (i === 0) {
+                matrix[j][i] = vectY[j]
+            } else {
+                matrix[j][i] = (matrix[j + 1][i - 1] - matrix[j][i - 1]) / (vectX[i + j] - vectX[j])
+            }
+            /* matrix[j][i] = num;
+            num++ */
         }
     }
-    // for (let i = 0; i < n; i++) {
-    //     //matrix[i][0] = (vectY[i + 1] - vectY[i]) / (vectX[i + 1] - vectX[i])
-    //     for (let j = 1; j < n - i - 1; j++) {
-    //         matrix[i][j] = (matrix[i + 1][j - 1] - matrix[i][j - 1]) / (vectX[i + 2] - vectX[i])
-    //         console.log(vectX)
-    //         console.log(`(${matrix[i + 1][j - 1]}-${matrix[i][j - 1]})/(${vectX[i + 2]}-${vectX[i]})`)
-    //         console.log(matrix)
-    //     }
-    // }
-    console.log(matrix)
+    //console.log(`${matrix[0]}\n${matrix[1]}\n${matrix[2]}\n${matrix[3]}`)
+
+    return matrix;
     //тут я поняла, что у нас равноотстоящие узлы...
     //но не стала переделывать
-
-    let newtonPoly = vectY[0];
-    for (let i = 0; i < n; i++) {
-        let member = matrix[0][i];
-        for (let j = 1; j < i + 1; j++) {
-            //member *= (x - vectX[i])
-            //прописать умножение многочленов
-        }
-        newtonPoly += member;
-        //member = 0;
-    }
-
 }
 
-defineNewtonPoly(points)
+function mult(a, b) {
+    const calc = new UniversalCalculator;
+    const members = [];
+    if (a.poly.length < b.poly.length) {
+        let glass = a;
+        a = b;
+        b = glass;
+    }
+    a.poly.forEach(elemA => {
+        b.poly.forEach(elemB => {
+            members.push(new Member(calc.mult(elemA.value, elemB.value), calc.add(elemA.power, elemB.power)));
+        });
+    });
+    //приведение подобных слагаемых:
+    const members2 = [];
+    for (let j = members[0].power; j > -1; j--) {
+        let power = [];         //коэффициенты подобных слагаемых
+        for (let i = 0; i < members.length; i++) {
+            if (members[i].power === j) {
+                power.push(members[i].value);
+            }
+        }
+        members2.push(new Member(power.reduce((sum, current) => sum + current, 0), j));
+    }
+    return new Polynomial(members2);
+}
+
+function defineNewtonPoly() {
+
+    let divDiffMatrix = defineDividedDifferences(points)
+    let xPolies = `(x - ${vectX[0]})`;
+    //let xPolies = ``;
+    let newtonPoly = `${vectY[0]}`;
+    for (let i = 1; i < n; i++) {
+        newtonPoly += ` + ${divDiffMatrix[0][i]}*${xPolies}`
+        xPolies += `*(x-${vectX[i]})`
+    }
+    return newtonPoly;
+    console.log(newtonPoly)
+}
+
+function defineYbyNewtonPoly(x) {
+    return eval(defineNewtonPoly()) 
+}
+
+    console.log(defineNewtonPoly())
+    console.log(defineYbyNewtonPoly(2))
+
