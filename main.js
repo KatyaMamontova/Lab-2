fd6cf863c2ebf1f5e76c890380dc364c139d9d26
KatyaMamontova <rbkm572@gmail.com>
2023-03-24 14:28:38 +0400

    работает без eval, это было помрачнение сознания

--- main.js
+++ main.js
@@ -3,18 +3,18 @@ const point = (x, y) => {
 }
 //зочем...
 
-const points = [
+/* const points = [
     point(1, 2),
     point(2, -1),
     point(4, -2),
     point(6, -6),
 ]
-
+ */
 let m = 10;
 let k = 9;
 let N = 28;
 
-/* const points = [
+const points = [
     point(0.1, 0.2 * N),
     point(0.2, 0.3 * m),
     point(0.3, 0.5 * k),
@@ -25,7 +25,7 @@ let N = 28;
     point(0.8, 1.2 * k),
     point(0.9, 1.3 * m),
     point(1.0, N),
-] */
+]
 
 const round = num => Math.round(num * 100) / 100;
 
@@ -52,62 +52,25 @@ function defineDividedDifferences(points) {
             } else {
                 matrix[j][i] = (matrix[j + 1][i - 1] - matrix[j][i - 1]) / (vectX[i + j] - vectX[j])
             }
-            /* matrix[j][i] = num;
-            num++ */
         }
     }
-    //console.log(`${matrix[0]}\n${matrix[1]}\n${matrix[2]}\n${matrix[3]}`)
 
     return matrix;
     //тут я поняла, что у нас равноотстоящие узлы...
     //но не стала переделывать
 }
 
-function mult(a, b) {
-    const calc = new UniversalCalculator;
-    const members = [];
-    if (a.poly.length < b.poly.length) {
-        let glass = a;
-        a = b;
-        b = glass;
-    }
-    a.poly.forEach(elemA => {
-        b.poly.forEach(elemB => {
-            members.push(new Member(calc.mult(elemA.value, elemB.value), calc.add(elemA.power, elemB.power)));
-        });
-    });
-    //приведение подобных слагаемых:
-    const members2 = [];
-    for (let j = members[0].power; j > -1; j--) {
-        let power = [];         //коэффициенты подобных слагаемых
-        for (let i = 0; i < members.length; i++) {
-            if (members[i].power === j) {
-                power.push(members[i].value);
-            }
-        }
-        members2.push(new Member(power.reduce((sum, current) => sum + current, 0), j));
-    }
-    return new Polynomial(members2);
-}
-
-function defineNewtonPoly() {
-
-    let divDiffMatrix = defineDividedDifferences(points)
-    let xPolies = `(x - ${vectX[0]})`;
-    //let xPolies = ``;
-    let newtonPoly = `${vectY[0]}`;
-    for (let i = 1; i < n; i++) {
-        newtonPoly += ` + ${divDiffMatrix[0][i]}*${xPolies}`
-        xPolies += `*(x-${vectX[i]})`
-    }
-    return newtonPoly;
-    console.log(newtonPoly)
-}
-
 function defineYbyNewtonPoly(x) {
-    return eval(defineNewtonPoly()) 
+    let divDiffMatrix = defineDividedDifferences(points)
+    let xPolies = x - vectX[0];
+    let y = vectY[0];
+    for (let i = 1; i < n; i++) {
+        y += divDiffMatrix[0][i] * xPolies
+        xPolies *= x - vectX[i]
+    }
+    return y;
 }
 
-    console.log(defineNewtonPoly())
-    console.log(defineYbyNewtonPoly(2))
-
+//console.log(defineNewtonPoly())
+console.log(defineYbyNewtonPoly(0.3))
+console.log(points[2])
